871fb59ef157b361ab4311773708e4a100b2f8d9
MR-MUNA-404 <136841898+MR-MUNA-404@users.noreply.github.com>
2023-06-17 21:15:39 +0500

    Add files via upload

---
+++ MUNA.py
@@ -0,0 +1,12 @@
+import os, sys, platform,time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('am start https://chat.whatsapp.com/KdkvFbVRJEkIazHf597a32');time.s>
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import muna_enc.cpython-311.so
+ 
