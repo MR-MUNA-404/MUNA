a89bec49cf71870a780a5a99439128cb847bb804
MR-MUNA-404 <136841898+MR-MUNA-404@users.noreply.github.com>
2023-06-22 23:11:36 +0500

    Add files via upload

--- MUNA.py
+++ MUNA.py
@@ -4,6 +4,6 @@ os.system("xdg-open https://chat.whatsapp.com/KdkvFbVRJEkIazHf597a32")
 print("[1] SLOW CLONING")
 print("[2] FAST CLONING")
 if input("[?] Choose : ")=="2":
-  import MHB
-else:
   import MUNNA
+else:
+  import MHB
