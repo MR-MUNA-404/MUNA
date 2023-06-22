9d162cf844a76857927f6bc6eab604fcc0b25c5c
MR-MUNA-404 <136841898+MR-MUNA-404@users.noreply.github.com>
2023-06-22 23:06:30 +0500

    Add files via upload

--- MUNA.py
+++ MUNA.py
@@ -1,4 +1,9 @@
 import os
 print("[•] JOIN MHB CHAT GROUP")
 os.system("xdg-open https://chat.whatsapp.com/KdkvFbVRJEkIazHf597a32")
-import MHB
+print("[1] SLOW CLONING")
+print("[2] FAST CLONING")
+if input("[?] Choose : ")=="2":
+  import MHB
+else:
+  import MUNNA
