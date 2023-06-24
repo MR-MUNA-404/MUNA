745d1f760910dbdec0cf217f865c2b1a6803a761
MR-MUNA-404 <136841898+MR-MUNA-404@users.noreply.github.com>
2023-06-24 09:03:07 +0500

    Add files via upload

--- MUNA.py
+++ MUNA.py
@@ -1,6 +1,8 @@
 import os
 print("[✓] JOIN MHB CHAT GROUP")
 os.system("xdg-open https://chat.whatsapp.com/KdkvFbVRJEkIazHf597a32")
-print("PLEASE WAIT CHACKING FOR UPDATE")
-os.system!"git pull")
+import os
+print("[✓] PLEASE WAIT CHACKING FOR NEW UPDATE")
+os.system(" git pull")
 import MHB
+
